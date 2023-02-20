36df9b5115d138003a675a45103f9fff64121a62
Gobezar <vadimkiselevv@yandex.ru>
2023-02-20 21:32:49 +0400

    sec commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const App = () => {
       <Navigation />
       <Routes>
       <Route path='/users' element={<UsersPage />}/>
-          <Route path='/' element={<UsersPage />}/>
-          <Route path='*' element={<UsersPage />}/>
+      <Route path='/' element={<UsersPage />}/>
+      <Route path='*' element={<UsersPage />}/>
       <Route path="/users/:id" element={<UserItemPage/>}/>
       <Route path='/todos' element={<TodosPage />}/>
       <Route path="/todos/:id" element={<TodosItemPage/>}/>
